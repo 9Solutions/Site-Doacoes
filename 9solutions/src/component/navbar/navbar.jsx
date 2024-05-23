4b40c0be54db259ee319b25e010acd8c09727a67
arthurdpaula <arthur.cornelio@sptech.school>
2024-05-23 09:11:48 -0300

    mudando imagem de local

--- 9solutions/src/component/navbar/navbar.jsx
+++ 9solutions/src/component/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./navbar.module.css";
-import logo from "../../utils/logo.png";
+import logo from "../../utils/img/logo.png";
 
 const navBar = () => {
   return (
